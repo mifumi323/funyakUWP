70652969f12507d9f4bbb25474066f4ff238871a
mifumi323 <mifumi323@tgws.fromc.jp>
2016-05-13 07:37:49 +0900

    FPSタイマー＆カウンター

---
+++ UWPView/FpsCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MifuminSoft.funyak.View.Utility
+{
+    public class FpsCounter
+    {
+        long frame = 0;
+        long count = 0;
+        long prevTime = 0;
+        double fps = 0.0;
+
+        /// <summary>経過フレーム数</summary>
+        public long Frame { get { return frame; } }
+        /// <summary>FPS</summary>
+        public double Fps { get { return fps; } }
+
+        public void Step()
+        {
+            frame++;
+            // FPS計測
+            if (count == 0)
+            {
+                count = 60;
+                long time = DateTime.Now.Ticks;
+                fps = 10000000.0 * count / (time - prevTime);
+                prevTime = time;
+            }
+            count--;
+        }
+    }
+}
---
+++ UWPView/FpsTimer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MifuminSoft.funyak.View.Utility
+{
+    public class FpsTimer
+    {
+        /// <summary>FPS</summary>
+        double fps = 60.0;
+        /// <summary>1フレームあたりの待機Tick数</summary>
+        long waitSpan = 0;
+        /// <summary>次回の時刻</summary>
+        long nextTime = 0;
+
+        /// <summary>FPSを設定または取得します</summary>
+        public double Fps
+        {
+            get { return fps; }
+            set
+            {
+                if (value < 0.0) throw new ArgumentOutOfRangeException();
+                fps = value;
+                if (fps == 0)
+                {
+                    waitSpan = 0;
+                }
+                else
+                {
+                    waitSpan = (long)(10000000 / fps);
+                }
+                ResetNextTime();
+            }
+        }
+
+        /// <summary>FPSタイマーを生成します</summary>
+        /// <param name="fps">1秒間のフレーム数</param>
+        public FpsTimer(double fps = 60.0)
+        {
+            Fps = fps;
+        }
+
+        public void ResetNextTime()
+        {
+            nextTime = DateTime.Now.Ticks + waitSpan;
+        }
+
+        /// <summary>次のフレーム開始時刻まで待機</summary>
+        public async Task Wait()
+        {
+            long waitTicks = nextTime - DateTime.Now.Ticks;
+            if (waitTicks > 0)
+            {
+                // 通常待機
+                await Task.Delay((int)(waitTicks / 10000));
+                nextTime += waitSpan;
+            }
+            else if (waitTicks > -waitSpan)
+            {
+                // 微妙に遅れているので待機なし
+                nextTime += waitSpan;
+            }
+            else
+            {
+                // 遅れすぎているのでFPS維持放棄
+                ResetNextTime();
+            }
+        }
+    }
+}
